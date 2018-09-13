d6c422c9b61438a830ea2c6dffd0066523fd97c3
mcbeem <mmcbee@gmail.com>
2018-09-13 09:56:56 -0400

    add credible interval to output

--- R/plotBayes_function.R
+++ R/plotBayes_function.R
@@ -7,6 +7,7 @@
 #' @param prior.parameters A vector of length two providing the parameter values for the prior. When prior.type="normal", the values are the mean and sd. When prior.type="uniform", the values are the min and the max.
 #' @param min The minimum possible value of mu to consider.
 #' @param max The maximum possible value of mu to consider.
+#' @param credible The width of the credible interval specified as a number between zero and one. Defaults to 0.95 for a 95% credible interval.
 #' @param points The number of values of mu to be calculated, higher means more precision. Defaults to 1000.
 #' @export
 #' @return Returns a list with the following components:  
@@ -14,14 +15,17 @@
 #'   \item \code{mean}    The sample mean of the data.  \cr 
 #'   \item \code{map}    The maximum a posteriori estimate of the mean.  \cr 
 #'   \item \code{eap}    The expected a posteriori estimate of the mean.  \cr
+#'   \item \code{credible.interval} The values defining the endpoints of the Bayesian credible interval.
 #'  }
 #' @examples
 #' set.seed(1)
 #' data <- rnorm(n=10, mean=1, sd=1)
 #' plotBayes(data, prior.type="normal", prior.parameters=c(0, .5), min=-2, max=2) 
 #' plotBayes(data, prior.type="uniform", prior.parameters=c(.7, 1.5), min=-2, max=2)
+#' plotBayes(data, prior.parameters=c(.7, 1.5), min=-2, max=2, credible=.9)
 
-plotBayes <- function(data, prior.type="normal", prior.parameters, min, max, points=1000) {
+plotBayes <- function(data, prior.type="normal", prior.parameters, min, max, points=1000,
+                      credible=.95) {
   
   # do some checks
   if (!(prior.type %in% c("normal", "uniform"))) {stop("prior.type must be one of \"normal\" or \"uniform\"")}
@@ -35,6 +39,8 @@ plotBayes <- function(data, prior.type="normal", prior.parameters, min, max, poi
   if (prior.type=="normal" & (min>prior.parameters[1])) {stop("min must be substantially less than the mean parameter of the prior (prior.parameters[1])")}
   if (prior.type=="normal" & (max<prior.parameters[1])) {stop("max must be substantially greater than the mean parameter of the prior (prior.parameters[1])")}
   if (prior.type=="normal" & (prior.parameters[2]<= 0)) {stop("The standard deviation parameter of the prior (prior.parameters[2]) must be greater than zero.")}
+  
+  if (credible <= 0 | credible >= 1) {stop("credible must be between zero and one")}
   
   # declare function for calculating the loglikelihood of the data given mu
   calc.LL <- function(data, mu, sd) {
@@ -82,11 +88,33 @@ plotBayes <- function(data, prior.type="normal", prior.parameters, min, max, poi
   legend("topleft", c("Prior", "Likelihood", "Posterior"), lty = c(2, 2, 1), 
          col = c("gray48", "red", "skyblue"), lwd = c(1, 1, 4), bty = "n")
 
+  # find credible interval locations
+  
+  # define cumulative density function
+  calc.cum.dens <- function(i) {
+    # calculate the cumulative density below the index value
+    return(sum(normalized.posterior[1:i])*width)
+  }
+  
+  # indexes is a vector of all the indexes of the posterior
+  indexes <- 1:length(normalized.posterior)
+  
+  # calculate the area below each value
+    cumulative.density <- sapply(indexes, calc.cum.dens)
+    
+  # find the limits
+  # percentile bounds given the supplied value of argument 'credible'
+  percentiles <- c((1-credible)/2, 1-(1-credible)/2)
+    # index of the credible interval lower limit
+  ll.index <- which(abs(cumulative.density-percentiles[1])==min(abs(cumulative.density-percentiles[1])))
+   # index of the credible interval upper limit
+  ul.index <- which(abs(cumulative.density-percentiles[2])==min(abs(cumulative.density-percentiles[2])))
   
   return(list(
     data.mean=mean(data),
     map=mus[which(normalized.posterior == max(normalized.posterior))],
-    eap=sum((normalized.posterior/sum(normalized.posterior)*mus))
+    eap=sum((normalized.posterior/sum(normalized.posterior)*mus)),
+    credible.intercal=c(mus[ll.index], mus[ul.index])
   ))
 }
 
